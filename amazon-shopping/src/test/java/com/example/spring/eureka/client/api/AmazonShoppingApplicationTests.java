415ccc7f098396a358f45b326e81a1ecdbd6161a
kmjenniferng <ka.man.jennifer.ng@gmail.com>
2021-11-22 17:18:30 -0500

    Added files for project

---
+++ amazon-shopping/src/test/java/com/example/spring/eureka/client/api/AmazonShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.eureka.client.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
